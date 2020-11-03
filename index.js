97296dc7afbe16c67d87f7ce0dfa86f2267e2fc8
hayakawasho <hayakawa@pnrm.co.jp>
2020-11-04 03:25:15 +0900

    test3: basicAuth

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const protect = require('static-auth');
-const safeCompare = require('safe-compare');
+const express = require('express')
+const helmet = require('helmet')
+const basicAuth = require('basic-auth-connect')
 
-const USER_NAME = process.env.USER_NAME || 'admin';
-const PASSWORD = process.env.PASSWORD || 'admin';
+const USER_NAME = process.env.USER_NAME || 'admin'
+const PASSWORD = process.env.PASSWORD || 'admin'
 
-const app = protect(
-  '/',
-  (username, password) => safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD), // timing attack 対策
-  {
-    directory: `${__dirname}/public`,
-    onAuthFailed: (res) => {
-      res.end('Authentication failed')
-    },
-  }
-)
+const app = express()
+
+app.use(helmet())
+app.use(basicAuth(USER_NAME, PASSWORD))
+app.use(express.static('public'))
 
 module.exports = app
