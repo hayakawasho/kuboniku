22c2cd5cbd4054f0cd3c40080515e4833544ccaa
hayakawasho <hayakawa@pnrm.co.jp>
2020-12-02 19:18:09 +0900

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || 5000);
 
 app.use(basicAuth(process.env.BASIC_ID || 'id', process.env.BASIC_PASSWORD || 'password'));
 
-app.use('/', express.static(__dirname + '/out'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function () {
   console.log('Node app is running at localhost:' + app.get('port'));
