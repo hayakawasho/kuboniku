0140c71c0c057aa6bd94e49156da51ca6426d00b
hayakawasho <hayakawa@pnrm.co.jp>
2020-12-04 22:56:50 +0900

    add react-spring

--- src/components/_works/entry.tsx
+++ src/components/_works/entry.tsx
@@ -2,16 +2,39 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from './entry.module.scss'
-
 import Utils from '~/foundation/utils/Utils'
+import { useSpring, animated } from 'react-spring'
 
-const Component = React.memo((props: { data; index }) => {
-  const { data, index } = props
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 40,
+  (x - window.innerWidth / 2) / 40,
+  1.06,
+]
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
+const Component = React.memo(({ data, index }: { data; index }) => {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: {
+      mass: 5,
+      tension: 350,
+      friction: 40,
+    },
+  }))
+
   return (
     <>
       <article className="o-grid__item" data-smooth-item>
         <Link href={'/works/' + data.node.slug}>
-          <a className={styles.entry}>
+          <animated.a
+            className={styles.entry}
+            onMouseMove={({ clientX: x, clientY: y }) =>
+              set({ xys: calc(x, y) })
+            }
+            onMouseLeave={() => set({ xys: [0, 0, 1] })}
+            style={{ transform: props.xys.interpolate(trans) }}
+          >
             <div
               className="c-aspect"
               style={{ backgroundColor: `${data.node.acf.themeColor}` }}
@@ -34,7 +57,7 @@ const Component = React.memo((props: { data; index }) => {
                 <h2 className={styles.heading}>{data.node.title}</h2>
               </div>
             </div>
-          </a>
+          </animated.a>
         </Link>
       </article>
     </>
