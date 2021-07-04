0cafd3fea9de44e1adb200db1b158740bc7aba48
hayakawasho <hayakawa@pnrm.co.jp>
2021-07-05 05:22:49 +0900

    add sentry

--- src/context/handle-error/index.ts
+++ src/context/handle-error/index.ts
@@ -1,16 +1,20 @@
 import { useCallback } from 'react';
 import constate from 'constate';
+// import { useSentry } from './useSentry';
 
 interface IError extends Error {
   code: number;
 }
 
 const useHandleHttpError = () => {
+  // const { captureException } = useSentry();
 
   const handleHttpError = useCallback((error) => {
     let errors: IError | null = null;
 
     if (error) {
+      // captureException(error.error, error.data)
+
       console.log({ error })
     }
 
---
+++ src/context/handle-error/useSentry.ts
@@ -0,0 +1,30 @@
+import * as Sentry from '@sentry/browser'
+
+const useSentry = () => {
+  const initSentry = () => {
+    Sentry.init({
+     // dsn: config.sentryDsn
+    })
+  }
+
+  const captureException = (error, errorInfo) => {
+    Sentry.withScope(scope => {
+      if (errorInfo) {
+        // first capture any additional info
+        Object.keys(errorInfo).forEach(key => {
+          scope.setExtra(key, errorInfo[key])
+        })
+      }
+
+      // capture the actual error that was thrown
+      Sentry.captureException(error)
+    })
+  }
+
+  return {
+    initSentry,
+    captureException
+  }
+}
+
+export { useSentry }
