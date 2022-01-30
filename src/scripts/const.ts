e1ce37e282d0209e6b7efd87b8680800522c039b
hayakawasho <hayakawa@pnrm.co.jp>
2022-01-30 16:36:08 +0900

    update

--- src/scripts/const.ts
+++ src/scripts/const.ts
@@ -1,4 +1,4 @@
-export const WP_API_BASE = 'http://api.kuboniku.com'
+export const WP_API_BASE = 'https://hykwsho.sub.jp/kuboniku/wp-json/'
 
 export const AFTER_PAGE_READY = Symbol()
 export const LOADING_PROGRESS = Symbol()
--- src/views/_data/wp.js
+++ src/views/_data/wp.js
@@ -1,2 +1,56 @@
+// @ts-nocheck
+const WP_API_BASE = 'https://hykwsho.sub.jp/kuboniku/wp-json/'
 const axios = require('axios')
-const API = 'http://localhost:8001/wp-json/wp/v2/posts?orderby=date&order=desc'
+
+module.exports = async () => {
+  const rawWorks = await axios.get(WP_API_BASE + 'wp/v2/posts', {
+    params: {
+      per_page: 99,
+      order: 'desc'
+    }
+  })
+
+  const metaWorks = rawWorks.data.map(i => {
+    const eyecatch = i.acf.eyecatch
+    const eyecatchMobile = i.acf.eyecatch_mobile
+
+    return {
+      id: i.id,
+      h1: i.title.rendered,
+      slug: i.slug,
+      createAt: i.date,
+      category: i.acf.category.name,
+      eyecatch: {
+        pc: {
+          w: eyecatch.width,
+          h: eyecatch.height,
+          aspect: eyecatch.height / eyecatch.width,
+          src: eyecatch.url
+        },
+        sp: {
+          w: eyecatchMobile.width,
+          h: eyecatchMobile.height,
+          aspect: eyecatchMobile.height / eyecatchMobile.width,
+          src: eyecatchMobile.url
+        }
+      },
+      color: i.acf.theme_color,
+      gallery: i.acf.gallery,
+      siteUrl: i.acf.url,
+      role: i.acf.role.map(j => j.name),
+    }
+  })
+
+  const rawProfile = await axios.get(WP_API_BASE + 'wp/v2/pages/490')
+
+  return {
+    works: {
+      total: rawWorks.headers['x-wp-total'],
+      items: metaWorks
+    },
+    profile: {
+      h1: rawProfile.data.title.rendered,
+      html:rawProfile.data.content.rendered
+    }
+  }
+}
