afff9a7064c25432cd20dc5efbde410ae57bc397
hayakawasho <hayakawa@pnrm.co.jp>
2022-02-03 03:49:01 +0900

    Update entry.ts

--- src/entry.ts
+++ src/entry.ts
@@ -3,13 +3,13 @@ import './styles/global.css'
 import 'virtual:windi.css'
 
 import { router } from '@/app/router'
-import { SceneManager } from '@/app/sceneManager'
+import { createSceneManager } from '@/app/sceneManager'
 import { DefaultPage, WorksIndexPage } from '@/components/pages'
 
 const isDev = process.env.NODE_ENV === 'development'
 
 function init() {
-  const sceneManager = SceneManager.create()
+  const sceneManager = createSceneManager()
 
   router
     .use('/works', _req => {
