b31fb09371dcf950e850f01c77a8fb29a5ee7564
hayakawasho <hayakawa@pnrm.co.jp>
2022-02-10 21:13:53 +0900

    move

--- src/scripts/modules.ts
+++ src/scripts/modules.ts
@@ -1,4 +1,4 @@
-export { default as Skewscroll } from './features/skewscroll'
-export { default as Sns } from './features/sns'
-export { default as Menu } from './features/menu'
-export { default as Glworld } from './features/glworld'
+export { default as Skewscroll } from './features/mod.skewscroll'
+export { default as Sns } from './features/mod.sns'
+export { default as Menu } from './features/mod.menu'
+export { default as Glworld } from './features/mod.glworld'
