8a3424d1bcdd1273c5d66695bc23f0df766420c5
hayakawasho <hayakawa@pnrm.co.jp>
2022-04-10 16:28:41 +0900

    update

--- src/scripts/components/Sns.svelte
+++ src/scripts/components/Sns.svelte
@@ -16,32 +16,35 @@
   const { useDOMRef } = getContext$()
   const { refs } = useDOMRef<Refs>(refKeySet('toggleTrigger', 'icon'))
 
-  let isOpen: boolean | undefined
+  let isOpen: boolean | undefined // 初期描画しないように初期値はundefinedにする
 
   $: isOpen && onOpen()
   $: isOpen === false && onClose()
 
   function onOpen() {
-    TWEEN.parallel(
-      TWEEN.tween(refs.toggleTrigger, 0.55, EASE._3_CubicInOut).rotation(90),
-      TWEEN.serial(
-        TWEEN.prop(refs.icon).y(20).opacity(0),
+    TWEEN.serial(
+      TWEEN.prop(refs.icon).y(20).opacity(0).style('visibility', 'visible'),
+      TWEEN.parallel(
+        TWEEN.tween(refs.toggleTrigger, 0.55, EASE._3_CubicInOut).rotation(90),
         TWEEN.lag(
           0.07,
           TWEEN.tween(refs.icon, 0.5, EASE._3_CubicOut).y(0).opacity(1)
         )
-      )
+      ).willChange()
     ).play()
   }
 
   function onClose() {
-    TWEEN.parallel(
-      TWEEN.tween(refs.toggleTrigger, 0.55, EASE._3_CubicInOut).rotation(0),
-      TWEEN.lagSort(
-        0.07,
-        REVERSE,
-        TWEEN.tween(refs.icon, 0.5, EASE._3_CubicOut).y(20).opacity(0)
-      )
+    TWEEN.serial(
+      TWEEN.parallel(
+        TWEEN.lagSort(
+          0.07,
+          REVERSE,
+          TWEEN.tween(refs.icon, 0.5, EASE._3_CubicIn).y(20).opacity(0)
+        ),
+        TWEEN.tween(refs.toggleTrigger, 0.55, EASE._3_CubicInOut).rotation(0)
+      ).willChange(),
+      TWEEN.prop(refs.icon).y(20).opacity(0).style('visibility', 'hidden')
     ).play()
   }
 
--- src/scripts/foundation/lib/TWEEN.ts
+++ src/scripts/foundation/lib/TWEEN.ts
@@ -1,5 +1,7 @@
 import { Tween24, Ease24, Sort24 } from 'tween24'
 
+Tween24.skipHello()
+
 const TWEEN = Tween24
 const EASE = Ease24
 const REVERSE = Sort24._Reverse
