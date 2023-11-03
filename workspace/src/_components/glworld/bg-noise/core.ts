3cb5b2c62a8f37d774ca1bad19c60e42cc7129b8
hayakawasho <hayakawa@pnrm.co.jp>
2023-11-04 03:07:45 +0900

    update: bg-noise

--- workspace/src/_components/glworld/bg-noise/core.ts
+++ workspace/src/_components/glworld/bg-noise/core.ts
@@ -2,136 +2,88 @@ import * as THREE from "three";
 import Post from "./post";
 import Sample from "./visuals/sample";
 
-const dpr = Math.min(window.devicePixelRatio, 1.5);
-
-const deg2rad = (n: number) => {
-  return (n * Math.PI) / 180;
-};
-
-export default class Gl {
-  // #scene!: THREE.Scene
-  #camera!: THREE.PerspectiveCamera;
-  #renderer!: THREE.WebGLRenderer;
-  // #canvas!: HTMLCanvasElement
-
+export class Bg {
   #visuals!: any[];
   #post!: Post;
 
   #transforms!: any[];
   #postactive!: boolean;
 
-  #state!: {
-    stopped: boolean;
-    resizing: boolean;
-    ww: number;
-    wh: number;
-  };
-
-  constructor(canvas: HTMLCanvasElement, ww: number, wh: number) {
-    this.#state = {
-      resizing: false,
-      stopped: false,
-      wh,
-      ww,
-    };
-
-    this.#setup(canvas);
-  }
-
-  #setup = (canvas: HTMLCanvasElement) => {
-    this.#renderer = new THREE.WebGLRenderer({
-      alpha: true,
-      canvas,
-    });
-
-    this.#renderer.setClearColor(0x000000, 0);
-    this.#renderer.setSize(this.#state.ww, this.#state.wh);
-    this.#renderer.setPixelRatio(dpr);
-
-    this.#camera = new THREE.PerspectiveCamera(45, 1280 / 720, 0.1, 1000);
-    this.#camera.position.z = 50;
+  constructor(
+    private _renderer: THREE.WebGLRenderer,
+    private _camera: THREE.PerspectiveCamera,
+    context: {
+      mq: "pc" | "sp";
+    }
+  ) {
+    const { mq } = context;
 
     this.#visuals = [
       new Sample({
-        camera: this.#camera,
+        camera: this._camera,
         color: 0x181818,
-        dpr,
+        dpr: 0.1,
       }),
       new Sample({
-        camera: this.#camera,
+        camera: this._camera,
         color: 0xcccccc,
-        dpr,
+        dpr: 0.1,
       }),
     ];
 
     this.#post = new Post();
+    this.#setSize();
 
-    this.#setSize(this.#state.ww, this.#state.wh);
-
-    const ops = {
-      post: {
-        alpha: 0.16,
-        amount: 0.07,
-        blur: 0.2,
-        clouds: true,
-        noise: true,
-        rad: 0.9,
+    const transforms = {
+      pc: {
+        post: {
+          alpha: 0.1,
+          amount: 0.08,
+          blur: 0.2,
+          clouds: true,
+          noise: true,
+          rad: 0.65,
+        },
+      },
+      sp: {
+        post: {
+          alpha: 0.16,
+          amount: 0.02,
+          blur: 0.4,
+          clouds: true,
+          noise: true,
+          rad: 0.8,
+        },
       },
     };
 
-    this.#transform(ops);
-  };
+    this.#transform(transforms[mq]);
+  }
 
   render = () => {
-    this.#renderer.render(this.#post.scene, this.#post.camera);
+    this._renderer.render(this.#post.scene, this.#post.camera);
     this.#post.render();
   };
 
-  resize(width: number, height: number) {
-    const state = this.#state;
-    state.resizing = true;
-    this.#setSize(width, height);
-    state.resizing = false;
-  }
+  resize = () => {
+    this.#setSize();
+  };
 
-  // private _resume() {
-  //   this.#state.stopped = false
-  // }
-  //
-  // private _pause() {
-  //   this.#state.stopped = true
-  // }
-
-  #transform(transforms: any) {
+  #transform = (transforms: any) => {
     this.#transforms = transforms;
     this.#postactive = transforms.post.clouds || transforms.post.noise;
 
-    if (this.#post) this.#post.transforms = transforms;
+    if (this.#post) {
+      this.#post.transforms = transforms;
+    }
 
     for (const visual of this.#visuals) {
       visual.transforms = transforms;
     }
+  };
 
-    if (this.#camera) {
-      const deskscreen = false;
-
-      this.#camera.position.z = deskscreen ? transforms.camera.dist : 3000;
-      this.#camera.far = deskscreen ? transforms.camera.far : 4500;
-      this.#camera.updateProjectionMatrix();
-    }
-  }
-
-  #setSize(width: number, height: number) {
-    const radFov = deg2rad(this.#camera.fov);
-
-    this.#camera.aspect = width / height;
-    this.#camera.position.z = (width * 0.5) / Math.tan(radFov * 0.5);
-
-    this.#camera.updateProjectionMatrix();
-
-    this.#renderer.setSize(width, height);
-
-    this.#post.width = this.#renderer.domElement.width;
-    this.#post.height = this.#renderer.domElement.height;
-  }
+  #setSize = () => {
+    this.#post.width = this._renderer.domElement.width;
+    this.#post.height = this._renderer.domElement.height;
+  };
 }
--- workspace/src/_components/glworld/index.ts
+++ workspace/src/_components/glworld/index.ts
@@ -1,35 +1,79 @@
+import { getGPUTier } from "detect-gpu";
 import { defineComponent, useDomRef } from "lake";
-import Gl from "./bg-noise/core";
+import { Bg } from "./bg-noise/core";
 import { useTick } from "@/_foundation/hooks";
+import { useWindowSize } from "@/_states/window-size";
+import * as THREE from "three";
+import type { AppContext } from "@/_foundation/type";
 
 export default defineComponent({
   name: "GlWorld",
-  setup() {
+  setup(el: HTMLElement, { mq }: Pick<AppContext, "mq">) {
     const { refs } = useDomRef<{ canvas: HTMLCanvasElement }>("canvas");
-    const ww = window.innerWidth;
-    const wh = window.innerHeight;
+    const { height, width } = el.getBoundingClientRect();
 
-    const gl = new Gl(refs.canvas, ww, wh);
-
-    const onResize = (width: number, height: number) => {
-      gl.resize(width, height);
+    const state = {
+      resizing: false,
     };
 
-    const addScene = () => {
-      console.log("addScene");
+    const renderer = new THREE.WebGLRenderer({
+      // alpha: true,
+      canvas: refs.canvas,
+    });
+
+    renderer.setClearColor(0x000000, 0);
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 1.5));
+
+    getGPUTier().then((result) => {
+      if (result.tier === 1) {
+        renderer.setPixelRatio(1);
+      }
+    });
+
+    const FOV = 60;
+    const calcCamDistance = (h: number) => {
+      const vFov = (FOV * Math.PI) / 180;
+      const fovRad = vFov * 0.5;
+      return (h * 0.5) / Math.tan(fovRad);
     };
 
-    const removeScene = () => {
-      console.log("removeScene");
-    };
+    const camera = new THREE.PerspectiveCamera(FOV, width / height, 0.1, 2000);
+    camera.position.z = calcCamDistance(height);
+
+    const bg = new Bg(renderer, camera, {
+      mq: mq.value,
+    });
+
+    const scene = new THREE.Scene();
+    const addScene = (child: THREE.Mesh) => scene.add(child);
+    const removeScene = (child: THREE.Mesh) => scene.remove(child);
 
     useTick(({ timestamp: _ }) => {
-      gl.render();
+      if (state.resizing) {
+        return;
+      }
+
+      renderer.render(scene, camera);
+      bg.render();
+    });
+
+    useWindowSize(({ aspect, wh, ww }) => {
+      state.resizing = true;
+
+      renderer.setSize(ww, wh);
+
+      camera.aspect = aspect;
+      camera.position.z = calcCamDistance(wh);
+      camera.updateProjectionMatrix();
+
+      bg.resize();
+
+      state.resizing = false;
     });
 
     return {
       addScene,
-      onResize,
       removeScene,
     };
   },
